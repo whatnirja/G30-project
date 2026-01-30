818d6bd753942b8c724328ace69a8121ffe955fe
nirja <nirjadabhi34@gmail.com>
2026-01-30 15:02:19 -0500

    first commit

---
+++ G30-project/G30_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  G30_projectApp.swift
+//  G30-project
+//
+//  Created by Nirja Dabhi on 2026-01-30.
+//
+
+import SwiftUI
+
+@main
+struct G30_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
