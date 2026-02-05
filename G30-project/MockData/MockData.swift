0e44605a8025caf51cf6ee8dbb4cd385aaf82962
Gia Nagpal <gianagpal08@gmail.com>
2026-02-05 02:21:30 -0500

    Feature: dashboard UI

--- G30-project/MockData/MockData.swift
+++ G30-project/MockData/MockData.swift
@@ -21,5 +21,41 @@ extension CityWeather {
         ],
         theme: .rainy
     )
+    
+    static let mockLondon = CityWeather(
+            city: "London",
+            temperature: "3°",
+            condition: "Heavy Rain",
+            highLow: "H: 16°  |  L: 8°",
+            hourly: [
+                .init(time: "Now", icon: "cloud.heavyrain.fill", temp: "3°", precip: "70%"),
+                .init(time: "2 AM", icon: "cloud.heavyrain.fill", temp: "2°", precip: "75%"),
+                .init(time: "3 AM", icon: "cloud.rain.fill", temp: "2°", precip: "60%")
+            ],
+            factors: [
+                .init(title: "Heavy Rain", icon: "cloud.heavyrain.fill"),
+                .init(title: "Flood Risk", icon: "water.waves"),
+                .init(title: "Strong Winds", icon: "wind")
+            ],
+            theme: .rainy
+        )
+
+        static let mockWindsor = CityWeather(
+            city: "Windsor",
+            temperature: "-9°",
+            condition: "Storm Incoming",
+            highLow: "H: 24°  |  L: -18°",
+            hourly: [
+                .init(time: "Now", icon: "cloud.bolt.rain.fill", temp: "-9°", precip: "55%"),
+                .init(time: "2 AM", icon: "cloud.bolt.rain.fill", temp: "-10°", precip: "60%"),
+                .init(time: "3 AM", icon: "cloud.snow.fill", temp: "-11°", precip: "40%")
+            ],
+            factors: [
+                .init(title: "Lightning", icon: "bolt.fill"),
+                .init(title: "Strong Winds", icon: "wind"),
+                .init(title: "Snow Squalls", icon: "cloud.snow.fill")
+            ],
+            theme: .storm
+        )
 }
 
